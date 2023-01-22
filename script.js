0bbc680b99bf7a1ea5148817ac7dbaf86ea7c015
Pradyumna Kapure <kapurechaitanya@gmail.com>
2023-01-22 11:56:25 +0530

    complete

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let txt = document.getElementById("evaluatedText");
+let count = document.getElementById("letterCount");
+
+txt.addEventListener("input" , caclLength)
+
+function caclLength() {
+    let word = txt.value;
+    let wordLength = word.length;
+    count.innerHTML = wordLength
+}
